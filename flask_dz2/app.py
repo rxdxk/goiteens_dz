8c80d1ccf6d6b667122af6cb829d90840ac963bf
rxdxk <rodionovkirill330@gmail.com>
2023-12-28 19:29:11 +0200

    commit

--- flask_dz2/app.py
+++ flask_dz2/app.py
@@ -1,6 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 
 app = Flask(__name__)
+DATABASE = 'yourdatabase.db'
+
+def get_db_connection():
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def create_table():
+    conn = get_db_connection()
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS users (
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   username TEXT NOT NULL,
+                   email TEXT NOT NULL)''')
+    conn.commit()
+    conn.close()
+
+create_table()
 
 @app.route('/')
 def index():
@@ -13,6 +32,32 @@ def page1():
 @app.route('/page2')
 def page2():
     return render_template('page2.html')
+
+@app.route('/add_user', methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+
+        conn = get_db_connection()
+        c = conn.cursor()
+        c.execute("INSERT INTO users (username, email) VALUES (?, ?)", (username, email))
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('index'))
+
+    return render_template('add_user.html')
+
+@app.route('/users')
+def users():
+    conn = get_db_connection()
+    c = conn.cursor()
+    c.execute("SELECT * FROM users")
+    users = c.fetchall()
+    conn.close()
+
+    return render_template('users.html', users=users)
 
 if __name__ == '__main__':
     app.run(debug=True)
